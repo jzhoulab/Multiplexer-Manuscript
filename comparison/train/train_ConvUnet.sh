53b067deaa98d0fadf774e8be41ae6ce825cdd22
DennisTang2000 <60992814+DennisTang2000@users.noreply.github.com>
2023-01-07 22:11:08 -0800

    Update train_ConvUnet.sh

--- comparison/train/train_ConvUnet.sh
+++ comparison/train/train_ConvUnet.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#SBATCH --partition=GPU4A100                                  # select partion GPU
-#SBATCH --nodes=1                                        # number of nodes requested by user
-#SBATCH --gres=gpu:4                                     # use generic resource GPU, format: --gres=gpu:[n], n is the number of GPU card
-#SBATCH --time=7-00:00:00                                # run time, format: D-H:M:S (max wallclock time)
+
 python -u train_ConvUnet.py
