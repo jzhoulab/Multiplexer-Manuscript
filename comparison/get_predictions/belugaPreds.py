22e88f1dac29bb7df355bcf16f78f221385c1acb
DennisTang2000 <60992814+DennisTang2000@users.noreply.github.com>
2023-01-11 20:47:59 -0500

    Update belugaPreds.py

--- comparison/get_predictions/belugaPreds.py
+++ comparison/get_predictions/belugaPreds.py
@@ -71,21 +71,7 @@ def log_fold(alt, ref):
     return np.log(top/bot)
 
 
-CHRS = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9',
-        'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17',
-        'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX','chrY']
 
-
-size = []    
-for i in CHRS:
-    length = len(genome[i])
-    size.append(length) 
-size_normalized = size/np.sum(size)
-
-
-
-
-    
 def encode_seq(seq):
     """
     returns an encoded sequence 
